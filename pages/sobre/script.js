d0316512118513434ef9845d1216bee4da775c59
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-09-16 01:35:37 -0300

    feat: continuação conteudo página home.
    criação do restante do conteudo de atividades recentes e materias mais estudadas, adição de efeitos slide-up e slide-right, inserção de gif na página de cadastro.

---
+++ pages/sobre/script.js
@@ -0,0 +1,9 @@
+const showCard = () => {
+    const cards = document.querySelectorAll(".card")
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            card.style.opacity = 1
+        }, index * 800);
+    })
+    console.log("teste")
+}
