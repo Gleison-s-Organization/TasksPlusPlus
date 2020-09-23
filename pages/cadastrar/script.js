3a6826e9f4c8499f1e24ec3f23db4bc8052ac92f
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-09-23 01:37:32 -0300

    feat: nova estrutura de pastas

--- pages/cadastrar/script.js
+++
@@ -1,5 +0,0 @@
-function openLoginForm() {
-    const element = document.getElementById("formPopup");
-    element.classList.toggle('visible');
-}
-
---
+++ src/scripts/global.js
@@ -0,0 +1,47 @@
+const user = {
+    name: "Tony Stark",
+    email: "iron-man@mcu.com",
+    password: "123456"
+}
+
+let isOpenLoginForm = false
+
+
+const userLoginPopUp = document.getElementById("formPopup");
+function openLoginForm() {
+    const userLoginPopUp = document.getElementById("formPopup");
+    userLoginPopUp.classList.toggle('visible');
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const formPopUp = document.querySelector(".formPopup");
+    const formLogin = document.querySelector("form.formPopupContainer");
+    const userProfileIcon = document.querySelector(".user-profile-icon > img");
+    function handleForm(event) {
+        event.preventDefault();
+        const formData = new FormData(document.querySelector('form.formPopupContainer'))
+        const datas = [...[...formData].map(c => c[1])]
+        const [email, password] = datas
+
+        const validations = [
+            user.email === email,
+            user.password === password,
+        ]
+        const isLoginValid = +validations.every(validation => validation)
+        const loginMessages = [
+            () => alert("Login was unsuccessful, please check your username and password"),
+            () => {
+                alert("Logged in successfully")
+                formPopUp.classList.toggle('visible')
+                userProfileIcon.src = "../../assets/icons/logged-in.svg"
+            }
+        ]
+        const message = loginMessages[isLoginValid]
+        message();
+    }
+    formLogin.addEventListener('submit', handleForm);
+})
+
+
