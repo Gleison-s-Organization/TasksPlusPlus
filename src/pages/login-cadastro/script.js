66bf01c3b5506b48bff1b6070d26da8eeda74575
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-09-26 03:34:44 -0300

    refactor: modificação toast-message
    toast-message movido para a tela de loguin-cadastro e seu aspector alterado.

--- src/pages/login-cadastro/script.js
+++ src/pages/login-cadastro/script.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const user = {
+    name: "Tony Stark",
+    avatar:
+      "https://pickaface.net/gallery/avatar/23556424_171128_2020_uqewt.png",
+    email: "iron-man@marvel.com",
+    password: "123456",
+  };
+
   const signUpButton = document.getElementById("signUp");
   const signInButton = document.getElementById("signIn");
   const container = document.getElementById("container");
+  const formLogin = document.getElementById("formLogin");
 
   signUpButton.addEventListener("click", () => {
     container.classList.add("right-panel-active");
@@ -10,4 +19,45 @@ document.addEventListener("DOMContentLoaded", () => {
   signInButton.addEventListener("click", () => {
     container.classList.remove("right-panel-active");
   });
+
+  const body = document.querySelector("body");
+  body.style.opacity = 1;
+
+  function handleForm(event) {
+    event.preventDefault();
+    const formData = new FormData(document.querySelector("#formLogin"));
+    const formDataEntryValues = [...[...formData].map((c) => c[1])];
+    const [email, password] = formDataEntryValues;
+
+    const validations = [user.email === email, user.password === password];
+    const isLoginValid = +validations.every((validation) => validation);
+    const loginMessages = [
+      () => {
+        const toastElement = document.querySelector(".toast.toast--red");
+        container.classList.add("shake");
+        toastElement.classList.add("show");
+        setTimeout(() => {
+          toastElement.classList.remove("show");
+        }, 2200);
+        setTimeout(() => {
+          container.classList.remove("shake");
+        }, 300);
+      },
+      () => {
+        const userInfo = document.querySelector(".user-info");
+        const toastElement = document.querySelector(".toast.toast--green");
+        toastElement.classList.add("show");
+        setTimeout(() => {
+          toastElement.classList.remove("show");
+        }, 3000);
+        formPopUp.classList.toggle("visible");
+        formPopUp.classList.remove("not-logged");
+        userInfo.classList.remove("not-logged");
+        userProfileIcon.src = "../../assets/icons/logged-in.svg";
+      },
+    ];
+    const message = loginMessages[isLoginValid];
+    message();
+  }
+  formLogin.addEventListener("submit", handleForm);
 });
