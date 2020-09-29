9b624727e4e84e062ad4e890038d5c6b59abf62f
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-09-28 21:05:03 -0300

    refactor: ajustes e criação da página index.

--- src/pages/cadastrar-atividade/script.js
+++ src/pages/cadastrar-atividade/script.js
@@ -53,4 +53,20 @@ document.addEventListener("DOMContentLoaded", () => {
     message();
   }
   activityForm.addEventListener("submit", handleForm);
+
+  // adiciona a classe hasItemSelected
+  const elements = [...document.querySelectorAll(".select-wrapper")];
+
+  elements.forEach((element) => {
+    const select = element.querySelector("select");
+    select.addEventListener("change", function (event) {
+      if (event.target.selectedIndex !== 0) {
+        element.classList.add("has-item-selected");
+      } else {
+        element.classList.remove("has-item-selected");
+      }
+      console.log(element);
+      console.log(select);
+    });
+  });
 });
