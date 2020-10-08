d55ed84956d23ff0fe3bc1114bbbdfdb71052654
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-10-08 01:34:19 -0300

    style: alteração da aplicação da opacity somente nos conteudos.

--- src/pages/login-cadastro/script.js
+++ src/pages/login-cadastro/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   const user = {
     name: "Tony Stark",
     avatar:
@@ -15,34 +15,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const formData = new FormData(document.querySelector("#formLogin"));
     const formDataEntryValues = [...[...formData].map((c) => c[1])];
     const [email, password] = formDataEntryValues;
+    window.location.href = "../home/home.html";
 
     // const validations = [user.email === email, user.password === password];
     const validations = [true];
     const isLoginValid = +validations.every((validation) => validation);
-    const loginMessages = [
-      () => {
-        const toastElement = document.querySelector(".js-toast-red");
-        container.classList.add("shake");
-        toastElement.classList.add("show");
-        setTimeout(() => {
-          toastElement.classList.remove("show");
-        }, 2200);
-        setTimeout(() => {
-          container.classList.remove("shake");
-        }, 300);
-      },
-      () => {
-        const userInfo = document.querySelector(".user-info");
-        const toastElement = document.querySelector(".js-toast-green");
-        toastElement.classList.add("show");
-        setTimeout(() => {
-          toastElement.classList.remove("show");
-        }, 3000);
-        window.location.href = "../home/home.html";
-      },
-    ];
-    const message = loginMessages[isLoginValid];
-    message();
+    // const loginMessages = [
+    //   () => {
+    //     const toastElement = document.querySelector(".js-toast-red");
+    //     container.classList.add("shake");
+    //     toastElement.classList.add("show");
+    //     setTimeout(() => {
+    //       toastElement.classList.remove("show");
+    //     }, 2200);
+    //     setTimeout(() => {
+    //       container.classList.remove("shake");
+    //     }, 300);
+    //   },
+    //   () => {
+    //     const userInfo = document.querySelector(".user-info");
+    //     const toastElement = document.querySelector(".js-toast-green");
+    //     toastElement.classList.add("show");
+    //     setTimeout(() => {
+    //       toastElement.classList.remove("show");
+    //     }, 3000);
+    //     window.location.href = "../home/home.html";
+    //   },
+    // ];
+    // const message = loginMessages[isLoginValid];
+    // message();
   }
   formLogin.addEventListener("submit", handleForm);
 });
--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,72 +1,25 @@
-let isOpenLoginForm = false;
-
-const userLoginPopUp = document.getElementById("formPopup");
-const buttonLogout = document.getElementById("buttonLogout");
-
-function openLoginForm() {
-  const elements = document.querySelectorAll(".user-info, .formPopup");
-  elements.forEach((c, i) => {
-    debugger;
-    if (
-      i === 1 &&
-      c.classList.contains("user-info") &&
-      c.classList.contains("not-logged")
-    )
-      return;
-    c.classList.toggle("visible");
-  });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const buttonLogout = document.getElementById("buttonLogout");
-  if (buttonLogout != null) {
-    buttonLogout.addEventListener("click", () => {
+const logout = () => {
+  const $buttonLogout = document.getElementById("buttonLogout");
+  if ($buttonLogout != null) {
+    $buttonLogout.addEventListener("click", () => {
       window.location.href = "../login-cadastro/login-cadastro.html";
     });
   }
-  const body = document.querySelector("body");
-  body.append();
-  body.style.opacity = 1;
-  const formPopUp = document.querySelector(".formPopup");
-  const formLogin = document.querySelector("form.formPopupContainer");
-  const userProfileIcon = document.querySelector(".user-profile-icon > img");
-  function handleForm(event) {
-    event.preventDefault();
-    const formData = new FormData(
-      document.querySelector("form.formPopupContainer")
-    );
-    const datas = [...[...formData].map((c) => c[1])];
-    const [email, password] = datas;
+};
 
-    const validations = [user.email === email, user.password === password];
-    const isLoginValid = +validations.every((validation) => validation);
-    const loginMessages = [
-      () => {
-        const toastElement = document.querySelector(".js-toast-red");
-        formPopUp.classList.add("shake");
-        toastElement.classList.add("show");
-        setTimeout(() => {
-          toastElement.classList.remove("show");
-        }, 2200);
-        setTimeout(() => {
-          formPopUp.classList.remove("shake");
-        }, 300);
-      },
-      () => {
-        const userInfo = document.querySelector(".user-info");
-        const toastElement = document.querySelector(".js-toast-green");
-        toastElement.classList.add("show");
-        setTimeout(() => {
-          toastElement.classList.remove("show");
-        }, 3000);
-        formPopUp.classList.toggle("visible");
-        formPopUp.classList.remove("not-logged");
-        userInfo.classList.remove("not-logged");
-        userProfileIcon.src = "../../assets/icons/logged-in.svg";
-      },
-    ];
-    const message = loginMessages[isLoginValid];
-    message();
-  }
-  // formLogin.addEventListener("submit", handleForm);
+const applyFadeOpacity = () => {
+  const body = document.querySelector("body");
+  body.style.opacity = 1;
+};
+
+const openUserInfo = () => {
+  const elements = document.querySelectorAll(".user-info");
+  elements.forEach((element) => {
+    element.classList.toggle("visible");
+  });
+};
+
+window.addEventListener("load", () => {
+  logout();
+  applyFadeOpacity();
 });
