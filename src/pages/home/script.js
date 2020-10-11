9e2bc3b7852ecac0de9116670e5977a91e5e91a6
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-10-10 22:45:33 -0300

    feat: estrutura home.

---
+++ src/pages/home/script.js
@@ -0,0 +1,21 @@
+(function () {
+    const $radios = document.querySelectorAll(".tab > input");
+    const $tabs = [...document.querySelectorAll(`[data-toggle^=tab]`)];
+    console.log($radios);
+    let previousRadio;
+
+    const toggleClass = ({ id }) => {
+      const $tab = $tabs.find((c) => c.attributes["data-toggle"].value === id);
+      $tab.classList.toggle("tab--active");
+    };
+
+    const activateTab = ($radio) => {
+      [previousRadio, $radio].forEach((c) => toggleClass(c));
+      previousRadio = $radio;
+    };
+
+    $radios.forEach((radio, index) => {
+      index === 0 && (radio.checked = true) && (previousRadio = radio);
+      radio.addEventListener("change", (event) => activateTab(event.target));
+    });
+  })();
