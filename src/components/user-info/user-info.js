74a2949fa79dc12cbbbf87e3b3e5e761030ac069
Gleison de Almeida Ribeiro <gleisonsubzerokz@gmail.com>
2020-10-16 15:13:37 -0300

    feat: finalização component user-info.

--- src/components/user-info/user-info.js
+++ src/components/user-info/user-info.js
@@ -15,13 +15,11 @@ class UserInfo extends HTMLElement {
 
     const $buttonLogout = self.querySelector(".js-button-logout");
     const $buttonEdit = self.querySelector(".js-button-edit");
-    $buttonLogout.addEventListener("click", () => {
-      console.log(this);
-      // window.location.href = "../login-cadastro/login-cadastro.html";
+    $buttonLogout?.addEventListener("click", () => {
+      window.location.href = "../login-cadastro/login-cadastro.html";
     });
-    $buttonEdit.addEventListener("click", () => {
-      console.log(this);
-      // window.location.href = "../login-cadastro/login-cadastro.html";
+    $buttonEdit?.addEventListener("click", () => {
+      window.location.href = "../perfil-usuario/perfil-usuario.html";
     });
   }
 }
