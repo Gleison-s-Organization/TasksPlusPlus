d26c49320b9d427cc3a19e970125d352a3f52ea2
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-10-19 18:57:54 -0300

    fix: form acitivities.

--- src/pages/estatisticas/script.js
+++ src/pages/estatisticas/script.js
@@ -47,3 +47,47 @@ window.addEventListener("load", () => {
   });
   barChart.render();
 });
+
+/* chart.js chart examples */
+
+// chart colors
+var colors = ["#007bff", "#28a745", "#333333", "#c3e6cb", "#dc3545", "#6c757d"];
+
+/* 3 donut charts */
+var donutOptions = {
+  cutoutPercentage: 85,
+  legend: { position: "bottom", padding: 5, labels: { pointStyle: "circle", usePointStyle: true } },
+};
+
+// donut 1
+var chDonutData1 = {
+  labels: ["Bootstrap", "Popper", "Other"],
+  datasets: [
+    {
+      backgroundColor: colors.slice(0, 3),
+      borderWidth: 0,
+      data: [74, 11, 40],
+    },
+  ],
+};
+
+var chDonut1 = document.getElementById("chDonut1");
+if (chDonut1) {
+  new Chart(chDonut1, {
+    type: "pie",
+    data: chDonutData1,
+    options: donutOptions,
+  });
+}
+
+// donut 2
+var chDonutData2 = {
+  labels: ["Wips", "Pops", "Dags"],
+  datasets: [
+    {
+      backgroundColor: colors.slice(0, 3),
+      borderWidth: 0,
+      data: [40, 45, 30],
+    },
+  ],
+};
