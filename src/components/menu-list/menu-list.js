304622214f2eafdff0d2193bb29b225ac28a8658
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-10-24 15:00:21 -0300

    style: identation.

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -6,16 +6,15 @@ class MenuList extends HTMLElement {
     const responseCSS = await fetch("/src/components/menu-list/menu-list.css");
 
     const componentCSS = await responseCSS.text();
+    const innerHTML = await responseHTML.text();
 
     const style = document.createElement("style");
     style.textContent = componentCSS;
 
-    // self.innerHTML = await responseHTML.text();
-    // self.appendChild(style);
-
-    const innerHTML = await responseHTML.text();
-    self.innerHTML = `<style>${style.innerHTML}</style>${innerHTML}`;
-    // self.appendChild(style);
+    self.innerHTML = `
+    <style>${style.innerHTML}</style>
+    ${innerHTML}
+    `;
   }
 }
 customElements.define("menu-list", MenuList);
