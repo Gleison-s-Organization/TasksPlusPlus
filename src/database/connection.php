c5a745c7914c09c44abd398bb8dbbb584ba04a82
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-12-05 09:24:01 -0300

    📦 refactor: udpate config to awardspace database.

--- src/database/connection.php
+++ src/database/connection.php
@@ -1,12 +1,13 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-// $password = "My5Ql@2020";
-$password = '';
+$databaseName = "3579585_tasks";
+$server = "fdb30.awardspace.net	";
+$user = "3579585_tasks";
+$password = '3579585_taskspl4spl4s';
 
 try {
-    $connection = new PDO("mysql:host=$server;dbname=tasks", $user, $password);
+    $connection = new PDO("mysql:host=$server;dbname=$databaseName", $user, $password);
+    phpAlert("Conectou ao banco", '');
     $connection->exec('set names utf-8');
 } catch (PDOException $e) {
     echo "Erro do PDO" . $e->getMessage();
